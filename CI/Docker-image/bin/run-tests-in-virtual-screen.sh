51238083c96517b501b47ab7fc77bd935890bc90
niko.lahtinen <niko.lahtinen@sitowise.com>
2020-12-08 16:24:44 +0200

    user debug

--- CI/Docker-image/bin/run-tests-in-virtual-screen.sh
+++ CI/Docker-image/bin/run-tests-in-virtual-screen.sh
@@ -11,7 +11,7 @@ then
         --outputDir $ROBOT_REPORTS_DIR \
         ${ROBOT_OPTIONS} \
         -v LiviUSER:$ROBOT_CRED_USR \
-        -v LiviPWD:$ROBOT_CRED_PWD \
+        -v LiviPWD:$ROBOT_CRED \
         -i tagi \
         $ROBOT_TESTS_DIR
 else
